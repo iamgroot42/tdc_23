20f3bb1fc9214db4b6779ff33c6160b80ac0c139
Michael Jerge <mj6ux@virginia.edu>
2023-10-01 19:16:12 -0400

    Added sequence classification

--- trojan_detection/detector/main.py
+++ trojan_detection/detector/main.py
@@ -7,27 +7,25 @@ from sklearn.model_selection import train_test_split
 from detector.utils import *
 
 def main():
-    data_dict = json_load_data('trojan_detection/data/dev/base/trojan_specifications_train_dev_base.json')
+    data_dict = json_load_data('path_to_your_file.json')
     data = []
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     
     for target, triggers in data_dict.items():
         for trigger in triggers:
-            # Consider 'target' and 'trigger' as two different sequences, concatenate them to create pairs
             encoded_pair = tokenizer(trigger + ' [SEP] ' + target, truncation=True, padding='max_length', max_length=512)
             data.append((encoded_pair, target))
     
     label_encoder = LabelEncoder()
     labels = label_encoder.fit_transform([item[1] for item in data])
-    
     tokenized_pairs = tokenizer.batch_encode_plus([(item[0]['input_ids']) for item in data], padding=True, return_tensors='pt')
     
     X_train, X_test, y_train, y_test = train_test_split(tokenized_pairs, labels, test_size=0.2, random_state=42)
     train_dataset = DataProcessor(X_train, y_train)
     test_dataset = DataProcessor(X_test, y_test)
     
-    # Model & Training
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    
     model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=len(label_encoder.classes_))
     model.to(device)
     
@@ -35,7 +33,7 @@ def main():
     optim = AdamW(model.parameters(), lr=5e-5)
     
     model.train()
-    for epoch in range(3):  # Adjust the number of epochs as needed
+    for epoch in range(3):
         for batch in train_loader:
             batch = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**batch)
